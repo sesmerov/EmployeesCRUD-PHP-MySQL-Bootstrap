341315a3083898f3d35a66b8724457e75321a8bb
Diego Sesmero <sesmerov@protonmail.com>
2025-01-06 04:24:01 +0100

    Añadido el código en config.example.php

---
+++ app/config.example.php
@@ -0,0 +1,6 @@
+<?php
+//Este fichero debe ser renombrado a config.php y los campos de DB_USER, DB_PASSWD y DATABASE modificados 
+define("SERVER_DB", "localhost");
+define("DB_USER", "your_username"); // Indicar usuario de la base de datos
+define("DB_PASSWD", "your_password"); // Indicar password de la base de datos
+define("DATABASE", "your_database"); // Indicar nombre de la base de datos
