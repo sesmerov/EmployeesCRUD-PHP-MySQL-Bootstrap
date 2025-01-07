7a63f50c3b51dd0187bbcab908a1df8a6cc631a3
Diego Sesmero <sesmerov@protonmail.com>
2025-01-07 02:38:45 +0100

    Modificado estilo de form.html. Ocultado campo id al añadir empleado

--- app/templates/form.php
+++ app/templates/form.php
@@ -18,50 +18,50 @@
     <main>
         <div class="container d-flex justify-content-center align-items-center mt-5">
             <form method="POST" class="p-4 border rounded shadow w-75">
-                <div class="mb-3 row ">
-                    <label for="id" class="col-sm-3 col-form-label">ID</label>
+                <div class="mb-3 row <?= $order == "Add" ? 'visually-hidden' : ''?> ">
+                    <label for="id" class="col-sm-3 col-form-label fw-bold">ID</label>
                     <div class="col-sm-9">
-                        <input type="text" class="form-control-plaintext" name="id" id="id" value="<?= $order == "Modify" ? "$employee->id": "" ?>" readonly>
+                        <input type="text" class="form-control-plaintext " name="id" id="id" value="<?= $order == "Modify" ? "$employee->id": "" ?>" readonly>
                     </div>
                 </div>
 
                 <div class="mb-3 row">
-                    <label for="first_name" class="col-sm-3 col-form-label">Nombre</label>
+                    <label for="first_name" class="col-sm-3 col-form-label fw-bold">Nombre</label>
                     <div class="col-sm-9">
                         <input type="text" class="form-control" name="first_name" id="first_name" value="<?= $order == "Modify" ? "$employee->first_name": "" ?>">
                     </div>
                 </div>
 
                 <div class="mb-3 row">
-                    <label for="last_name" class="col-sm-3 col-form-label">Apellido</label>
+                    <label for="last_name" class="col-sm-3 col-form-label fw-bold">Apellido</label>
                     <div class="col-sm-9">
                         <input type="text" class="form-control" name="last_name" id="last_name" value="<?= $order == "Modify" ? "$employee->last_name": "" ?>">
                     </div>
                 </div>
 
                 <div class="mb-3 row">
-                    <label for="phone" class="col-sm-3 col-form-label">Teléfono</label>
+                    <label for="phone" class="col-sm-3 col-form-label fw-bold">Teléfono</label>
                     <div class="col-sm-9">
                         <input type="text" class="form-control" name="phone" id="phone" value="<?= $order == "Modify" ? "$employee->phone": "" ?>">
                     </div>
                 </div>
 
                 <div class="mb-3 row">
-                    <label for="email" class="col-sm-3 col-form-label">Correo Electrónico</label>
+                    <label for="email" class="col-sm-3 col-form-label fw-bold">Correo Electrónico</label>
                     <div class="col-sm-9">
                         <input type="email" class="form-control" name="email" id="email" value="<?= $order == "Modify" ? "$employee->email": "" ?>">
                     </div>
                 </div>
 
                 <div class="mb-3 row">
-                    <label for="department" class="col-sm-3 col-form-label">Departamento</label>
+                    <label for="department" class="col-sm-3 col-form-label fw-bold">Departamento</label>
                     <div class="col-sm-9">
                         <input type="text" class="form-control" name="department" id="department" value="<?= $order == "Modify" ? "$employee->department": "" ?>">
                     </div>
                 </div>
 
                 <div class="mb-3 row">
-                    <label for="job_title" class="col-sm-3 col-form-label">Puesto</label>
+                    <label for="job_title" class="col-sm-3 col-form-label fw-bold">Puesto</label>
                     <div class="col-sm-9">
                         <input type="text" class="form-control" name="job_title" id="job_title" value="<?= $order == "Modify" ? "$employee->job_title": "" ?>">
                     </div>
